ad0c5f65e29cb7f4071c748a3161150bd0194be2
GHarshitha115 <54620828+GHarshitha115@users.noreply.github.com>
2020-05-06 20:32:09 +0530

    Update Exam5th.py

--- Exam5th.py
+++ Exam5th.py
@@ -4,13 +4,45 @@ Q3="SELECT Min(rank) FROM Movie WHERE year=1991"
 Q4="SELECT fname,lname FROM Actor INNER JOIN Cast ON id=pid WHERE mid=27"
 Q5="SELECT COUNT(mid) FROM Actor INNER JOIN Cast ON id=pid WHERE fname='Jon' AND lname='Dough'"
 Q6="SELECT name FROM Movie WHERE year BETWEEN 2003 AND 2006 AND name Like 'Young Latin Girls%'"
-#Q7="SELECT DISTINCT fname,lname FROM Director INNER JOIN Movie ON `Director`.id=`Movie`.id HAVING COUNT(`Director`.id)>=1 WHERE name Like'Star Trek%'"
-Q7="SELECT DISTINCT fname,lname FROM Director INNER JOIN Movie WHERE(SELECT COUNT(id) FROM Director)>=1 AND `Movie`.name Like'Star Trek%'"
-#Q8="SELECT COUNT(`Movie`.id) FROM Actor INNER JOIN Director ON `Actor`.id=`Movie`.id WHERE fname='Jackie (I)' AND lname='Chan'"
-Q8="SELECT COUNT(`Actor`.id) FROM Actor,Movie WHERE `Actor`.fname='Jackie (I)' AND lname='Chan' AND `Actor`.id=`Movie`.id ORDER BY `Movie`.name ASC"
-#Q9="SELECT fname,lname FROM Director INNER JOIN Movie ON `Director`.id=`Movie`.id WHERE year=2001 ORDER BY fname ASC,lname DESC"
+Q7="SELECT fname,lname FROM MovieDirector INNER JOIN Director ON `MovieDirector`.did=`Director`.id INNER JOIN Movie ON `MovieDirector`.mid=`Movie`.id WHERE name Like 'Star Trek%'"
+Q8='''SELECT name FROM Movie
+INNER JOIN Cast ON 
+`Movie`.id=`Cast`.mid 
+INNER JOIN Actor ON 
+ `Actor`.id=`Cast`.pid
+INNER JOIN MovieDirector ON
+`Movie`.id=`MovieDirector`.mid
+INNER JOIN Director ON
+`Director`.id=`MovieDirector`.did
+WHERE Actor.fname='Jackie (I)' AND Actor.lname='Chan' AND
+Director.fname='Jackie (I)' AND Director.lname='Chan'
+'''
+Q9="SELECT fname,lname FROM Director INNER JOIN MovieDirector ON `MovieDirector`.did=`Director`.id INNER JOIN Movie ON `MovieDirector`.mid=`Movie`.id WHERE year = 2001 GROUP BY did HAVING COUNT(mid)>=4 ORDER BY fname ASC,lname DESC"
 Q10="SELECT gender,Count(gender) FROM Actor GROUP BY gender ORDER BY gender ASC"
-Q11=""
+Q11='''
+SELECT DISTINCT m1.name,m2.name,m1.rank,m1.year FROM Movie AS m1,Movie AS m2 
+WHERE (m1.year=m2.year AND m1.rank=m2.rank) AND m1.name<>m2.name
+ORDER BY m1.name ASC Limit 100
+'''
+Q12='''
+SELECT fname,year,AVG(rank) FROM Cast INNER JOIN Actor ON 
+`Actor`.id=`Cast`.pid INNER JOIN Movie ON `Movie`.id=`Cast`.mid 
+GROUP BY year,`Actor`.id ORDER BY fname ASC,year DESC Limit 100;
+'''
+Q13='''
+SELECT Actor.fname,Director.fname,AVG(rank) as score FROM 
+Actor INNER JOIN Cast ON
+`Cast`.pid=`Actor`.id
+INNER JOIN Movie ON
+`Cast`.mid=`Movie`.id
+INNER JOIN MovieDirector ON 
+`MovieDirector`.mid=`Movie`.id
+INNER JOIN Director ON
+`Director`.id=`MovieDirector`.did
+GROUP BY `Actor`.id,`Director`.id 
+HAVING COUNT(`Movie`.id)>=5
+ORDER BY score DESC,Director.fname ASC,Actor.fname DESC Limit 100;
+'''
 
 
 
