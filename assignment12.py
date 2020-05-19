47c0a5b754f7b7a92f6ca96e8c4fef4a8533b4e3
GHarshitha115 <54620828+GHarshitha115@users.noreply.github.com>
2020-05-19 20:05:13 +0530

    Update assignment12.py

--- assignment12.py
+++ assignment12.py
@@ -16,3 +16,29 @@ class Student:
         return b
 student_obj=Student.get("ID=100")
 print(student_obj.name)
+
+
+----------------------------------------
+
+
+class Student:
+    def __init__(self,id=None,name=None,age=None,score=None):
+        self.name=name
+        self.id=id
+        self.age=age 
+        self.score=score
+    @staticmethod
+    def get(sql):
+        import sqlite3
+        #connection=sqlite3.connect("dbms/dbms_resources/students_db.sqlite3")
+        connection=sqlite3.connect("dbms/dbms_submissions/dbms_assignment_012/db.sqlite3")
+        crsr=connection.cursor()
+        crsr.execute("SELECT * from Student where student_id=3;")    
+        ans=(crsr.fetchone())
+        b=Student(*ans)
+        connection.close()
+        return b
+student_obj=Student.get("student_id=2")
+print(student_obj.name)
+print(student_obj.age)
+print(student_obj.score)
